bd37b691ae7411ac037bb425c9e05ff765b86897
K750i <munkin80@gmail.com>
2023-03-02 13:20:02 +0800

    Complete the 7 states transition

--- script.js
+++ script.js
@@ -14,9 +14,8 @@ const states = {
     FIRST_NUM_WITH_DECIMAL: 3,  // number w/ decimal is displayed
     SECOND_NUM: 4,  // number w/o decimal that comes after an operator key was pressed
     SECOND_NUM_WITH_DECIMAL: 5, // number w/ decimal that comes after an operator key was pressed
-    SECOND_NUM_DEC_START: 6,    // decimal key is pressed right after an operator key
-    OP: 7,  // an operator key is pressed
-    EQUALS: 8   // equal key is pressed
+    OP: 6,  // an operator key is pressed
+    EQUALS: 7,   // equal key is pressed
 };
 let currentState = states.START;
 let disp;
@@ -73,17 +72,74 @@ buttons.addEventListener('click', e => {
                 updateDisplay(disp);
                 currentState = states.SECOND_NUM;
             }
+            if (btnType === 'operator') {
+                operatorKey = btnValue;
+            }
+            if (btnType === 'key_dot') {
+                disp = '0.';
+                updateDisplay(disp);
+                currentState = states.SECOND_NUM_WITH_DECIMAL;
+            }
+            if (btnType === 'key_eq') {
+                disp = operations[operatorKey](disp, firstOperand);
+                updateDisplay(disp);
+                currentState = states.EQUALS;
+            }
             break;
         case states.SECOND_NUM:
             if (btnType === 'number') {
                 disp += btnValue;
                 updateDisplay(disp);
             }
+            if (btnType === 'operator') {
+                disp = operations[operatorKey](firstOperand, disp);
+                updateDisplay(disp);
+                operatorKey = btnValue;
+                firstOperand = disp;
+                currentState = states.OP;
+            }
+            if (btnType === 'key_dot') {
+                disp += '.';
+                updateDisplay(disp);
+                currentState = states.SECOND_NUM_WITH_DECIMAL;
+            }
             if (btnType === 'key_eq') {
                 secondOperand = disp;
                 disp = operations[operatorKey](firstOperand, secondOperand);
                 updateDisplay(disp);
                 currentState = states.EQUALS;
+            }
+            break;
+        case states.SECOND_NUM_WITH_DECIMAL:
+            if (btnType === 'number') {
+                disp += btnValue;
+                updateDisplay(disp);
+            }
+            if (btnType === 'operator') {
+                disp = operations[operatorKey](firstOperand, disp);
+                updateDisplay(disp);
+                operatorKey = btnValue;
+                firstOperand = disp;
+                currentState = states.OP;
+            }
+            if (btnType === 'key_eq') {
+                secondOperand = disp;
+                disp = operations[operatorKey](firstOperand, disp);
+                updateDisplay(disp);
+                currentState = states.EQUALS;
+            }
+            break;
+        case states.EQUALS:
+            if (btnType === 'key_eq') {
+                secondOperand = secondOperand ?? firstOperand;
+                disp = operations[operatorKey](disp, secondOperand);
+                updateDisplay(disp);
+            }
+            if (btnType === 'operator') {
+                firstOperand = disp;
+                secondOperand = null;
+                operatorKey = btnValue;
+                currentState = states.OP;
             }
             break;
         default:
